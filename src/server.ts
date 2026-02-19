b9aef6903bccc138a377771ccb5cc904fd96bbae
Md:Mosaraf Hossen <151431976+mosaraf68500@users.noreply.github.com>
2026-02-20 00:30:13 +0600

    delete

--- src/server.ts
+++ src/server.ts
@@ -142,6 +142,33 @@ app.put("/users/:id", async (req: Request, res: Response) => {
     });
   }
 });
+
+
+app.delete("/users/:id", async (req: Request, res: Response) => {
+  try {
+    const result = await pool.query(`DELETE FROM users WHERE id=$1`, [
+      req.params.id,
+    ]);
+
+    if (result.rows.length === 0) {
+      res.status(404).json({
+        success: false,
+        message: "result not found!",
+      });
+    } else {
+      res.status(200).json({
+        success: true,
+        message: "user found successfully",
+        data: null,
+      });
+    }
+  } catch (err: any) {
+    res.status(500).json({
+      success: false,
+      message: err.message,
+    });
+  }
+});
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
